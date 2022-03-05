fda18a035db7bc179a5eac5b23ad7d4cdb3cfeaa
merlin <ishe0004@algonquinlive.com>
2022-03-04 20:30:47 -0500

    Calculator app Javafx

---
+++ src/main/java/com/example/mvc/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.mvc;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class HelloController {
+    @FXML
+    private Text output;
+    @FXML
+    private void processNumpad(ActionEvent event){
+
+    }
+    @FXML
+    private void processOperator(ActionEvent event){
+
+    }
+
+
+}
